a778402dda0bc56cc5080b80a77f72d015c5a8b6
Likhith <likhithpbk@gmail.com>
2026-01-25 00:06:29 +0530

    LOGIN-API-IN-FRONTEND

--- NODE-FRONTEND/src/Login.jsx
+++ NODE-FRONTEND/src/Login.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from "axios"
 const Login = () => {
+const [emailID,setEmailID] = useState("");
+const [password,setPassword] = useState("")
+const handlesubmit = async() =>
+{
+  try{ 
+    const res =  await axios.post("http://localhost:8888/login"
+,{
+    emailID,
+    password
+  })}
+  catch(err)
+  {
+    console.log(err)
+  }
+
+}
   return (
-    <div>Login</div>
+    <div className='flex justify-center my-20'>
+      <div className="card card-dash bg-base-300 w-96">
+  <div className="card-body">
+    <h2 className="card-title justify-center">Login😊</h2>
+
+<div>
+  <fieldset className="fieldset">
+  <span className="fieldset-legend">EmailID :</span>
+  <input type="text" value={emailID} className="input"onChange={(e)=>setEmailID(e.target.value)} />
+  
+  </fieldset>
+   <fieldset className="fieldset">
+  <legend className="fieldset-legend">Password :</legend>
+  <input type="password" value={password} className="input" onChange={(e)=>setPassword(e.target.value)}/>
+  </fieldset>
+</div>
+<br/>
+
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary"onClick={handlesubmit}>Login</button>
+    </div>
+  </div>
+</div>
+    </div>
   )
 }
 
